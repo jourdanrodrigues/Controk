c654319063d341f218085ce596b3566f5ff989ae
Thiago Jourdan <thiagojourdan@gmail.com>
2015-07-12 13:29:45 -0300

    Função de pesquisa migrada
    Migração de função padrão de pesquisa no banco para "Connections.php"
    pelo fato de que todas as classes herdam dela.

--- php/class/Connection.php
+++ php/class/Connection.php
@@ -16,5 +16,16 @@ public class connection{
 	public function desconectar($mysqli){
 		mysqli_close($mysqli);
 	}
+	function getValueInBank($campo,$tabela,$campoPesquisa,$pesquisa){
+		$getValue='select '.$campo.' from '.$tabela;
+		if($campoPesquisa!=""||$pesquisa!=""){
+			$getValue.='where '.$campoPesquisa.' = '.$pesquisa.';';
+		}else{
+			$getValue.=';';
+		}
+		$gotValue=mysqli_query($mysqli,$getValue);
+		$value=mysqli_fetch_row($gotValue);
+		return $value[0];
+	}
 }
 ?>
